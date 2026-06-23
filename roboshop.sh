74f963788b6b1821674ec70c3c8e97dfad0dacfd
Thoyaz <thoyazthoyazpc@gmail.com>
2026-06-23 07:49:25 +0530

    done with roboshop server creation script

--- roboshop.sh
+++ roboshop.sh
@@ -32,7 +32,7 @@ do
         IP=$( 
             aws ec2 describe-instances \
             --instance-ids $INSTANCE_ID \
-            --query "Reservations[].Instances[].PublicIpAddress" \
+            --query "Reservations[].Instances[].PrivateIpAddress" \
             --output text 
         )
 
